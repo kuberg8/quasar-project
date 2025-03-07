15077ea33f6c67cb99eacec6d9d0c199e0cb704d
Алексей Демиденко <kuberg8@gmail.com>
2025-03-07 13:45:07 +0500

    added other page

---
+++ src/pages/SquarePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="block" />
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.block {
+  margin: auto;
+  height: 100px;
+  aspect-ratio: 1/1;
+  background-color: black;
+}
+</style>
